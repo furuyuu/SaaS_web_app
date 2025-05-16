c19bd855b778d4947d998d85268c13aaf794ca44
furuyuu <nyds102@yahoo.co.jp>
2025-05-16 20:57:25 +0900

    Initial commit

---
+++ src/app/api/generate-image/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+import FormData from "form-data";
+
+export async function POST(req: Request) {
+    const { keyword } = await req.json();
+    console.log("keyword", keyword)
+    try {
+        const payload = {
+            prompt: "Lighthouse on a cliff overlooking the ocean",
+            output_format: "png"
+        };
+
+        const formData = new FormData();
+        formData.append("prompt", payload.prompt);
+        formData.append("output_format", payload.output_format);
+
+        const response = await axios.postForm(
+            `https://api.stability.ai/v2beta/stable-image/generate/core`,
+            formData,
+            {
+                validateStatus: undefined,
+                responseType: "arraybuffer",
+                headers: { 
+                    Authorization: `Bearer ${process.env.STABILITY_API_KEY}`, 
+                    Accept: "image/*" 
+                },
+            },
+        );
+
+        if(response.status !== 200) {
+            throw new Error(`API error: ${response.status}`)
+        }
+
+        console.log(response.data);
+        return NextResponse.json(response.data)
+    } catch (error) {
+        console.error("Error generate image:", error);
+        return NextResponse.json(
+        {
+            error: "Faild to generate image"
+        },
+        { status: 500 }
+        );
+    }
+}
---
+++ src/components/dashboard/tools/image-generator.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { GenerateImageState } from '@/types/actions';
+import React, { useActionState } from 'react'
+
+const initialState: GenerateImageState = {
+    status: "idle",
+};
+
+const ImageGenerator = () => {
+    const [state, formAction] = useActionState(generateImage, initialState);
+
+    async function generateImage(formData: FormData) {
+        // "use server";
+        const keyword = formData.get("keyword");
+
+        try {
+            await fetch(`${process.env.BASE_URL}/api/generate-image`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ keyword}),
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div className="space-y-6">
+            <div className="space-y-4">
+                <form action={generateImage} className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="keyword">キーワード</Label>
+                        <Input
+                            id="keyword"
+                            name="keyword"
+                            placeholder="作成したい画像のキーワードを入力（例：海、山、都会）"
+                            required
+                        />
+                        
+                    </div>
+                    {/* submit button */}
+                    <Button>画像を生成する</Button>
+                </form>
+            </div>
+            {/* image preview */}
+            <div></div>
+        </div>
+    )
+}
+
+export default ImageGenerator
